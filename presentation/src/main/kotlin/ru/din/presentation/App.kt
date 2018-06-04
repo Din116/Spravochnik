2260f6ea51fbc99dbee5a07d6e803fa3850d33ab
Arkadiy <firecat1987@gmail.com>
2018-06-04 14:30:57 +0300

    Добавил main и login activity. Обновил версии зависимостей. Реализовал авторизацию и логаут на основе VKSdk.

---
+++ presentation/src/main/kotlin/ru/din/presentation/App.kt
@@ -0,0 +1,26 @@
+package ru.din.presentation
+
+import android.app.Application
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
+import com.vk.sdk.VKAccessToken
+import com.vk.sdk.VKAccessTokenTracker
+import com.vk.sdk.VKSdk
+
+class App : Application() {
+    var vkAccessTokenTracker: VKAccessTokenTracker = object : VKAccessTokenTracker() {
+        override fun onVKAccessTokenChanged(oldToken: VKAccessToken?, newToken: VKAccessToken?) {
+            if (newToken == null) {
+                startActivity(Intent(applicationContext, LoginActivity::class.java).addFlags(FLAG_ACTIVITY_NEW_TASK))
+
+            }
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        vkAccessTokenTracker.startTracking()
+        VKSdk.initialize(this)
+    }
+
+}
